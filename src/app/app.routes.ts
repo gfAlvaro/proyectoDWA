11b2f4734acd6e8147bf8769e22d253207cd2dc5
Álvaro García Fuentes <gf_alvaro@hotmail.com>
2026-07-16 23:59:54 +0200

    añadida estructura de header home footer y página de sesión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { SessionForm } from './session-form/session-form';
+import { App } from './app';
+import { Home } from './home/home';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: Home },
+    { path: 'iniciar-sesion', component: SessionForm }
+];
